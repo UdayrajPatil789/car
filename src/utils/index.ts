2420e45b537a8007095395ee330b5f6ec1f6a581
UdayrajPatil789 <udayrajp@fermion.in>
2024-03-08 16:26:11 +0530

    first commit

---
+++ src/utils/index.ts
@@ -0,0 +1,31 @@
+export async function fetchCars() {
+  const headers = {
+    "X-RapidAPI-Key": "4a84f3e33bmshc5203b49df57479p1699c4jsncd30c0070647",
+    "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+  };
+  const response = await fetch(
+    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla",
+    { headers: headers }
+  );
+  const result = await response.json();
+  console.log(result);
+  return result;
+  
+}
+
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+  const basePricePerDay = 50; 
+  const mileageFactor = 0.1; 
+  const ageFactor = 0.05; 
+
+  
+  const mileageRate = city_mpg * mileageFactor;
+  const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+  
+  const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+
+  return rentalRatePerDay.toFixed(0);
+};
+
